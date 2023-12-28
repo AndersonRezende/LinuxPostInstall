524242f814eb51bc3e99eeb8254b90eba7160bd2
= <=>
2023-12-28 15:40:49 -0300

    Melhora organização dos snaps

---
+++ scripts/apache.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Configuração do apache
+######### APACHE #########
+sudo ufw app list
+sudo ufw allow in "Apache"
+sudo systemctl restart apache2
+response=$(curl --write-out '%{http_code}' --silent --output /dev/null http://localhost)
+if [[ "$response" -ne 200 ]]
+then
+    echo "Houve um erro ao instalar o apache"
+    exit 1
+fi
+######### APACHE #########
---
+++ scripts/mysql.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Configuração do MySql
+######### MYSQL #########
+PASSWORD='root'
+sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$PASSWORD';"
+sudo mysql -p"$PASSWORD" -e "flush privileges;"
+######### MYSQL #########
