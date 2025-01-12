ca944820e73f57ec73795b3de59c579a6e5ff3ad
Anderson Rezende <andersonrezende17@hotmail.com>
2025-01-12 13:53:33 -0300

    Corrige diretório de músicas para link simbólico

--- scripts/commons/5-Symbolic_links.sh
+++ scripts/commons/5-Symbolic_links.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-######### PARTIÇÃO DE ARQUIVOS #########
+######### LINK SIMBÓLICO DE ARQUIVOS #########
 echo "[CONFIGURANDO] - Criação de links simbólicos"
 ln -s /mnt/Arquivos/Documentos /home/$USER/Documentos
-ln -s /mnt/Arquivos/Músicas /home/$USER/Música
+ln -s /mnt/Arquivos/Músicas /home/$USER/Músicas
 ln -s /mnt/Arquivos/Vídeos /home/$USER/Vídeos
 ln -s /mnt/Arquivos/Imagens /home/$USER/Imagens
 ln -s /mnt/Arquivos/Downloads /home/$USER/Downloads
 ln -s /mnt/Arquivos/Documentos/Projetos /home/$USER/Projetos
 echo "[CONFIGURANDO] - Criação de links simbólicos"
-######### PARTIÇÃO DE ARQUIVOS #########
+######### LINK SIMBÓLICO DE ARQUIVOS #########
