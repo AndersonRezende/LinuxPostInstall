845c85b9011fb88f86ae8136218665b4f7598bac
Anderson Rezende <andersonrezende17@hotmail.com>
2025-02-10 23:55:16 -0300

    Adiciona Rust

---
+++ scripts/commons/7-Rust.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "Instalando RUST"
+
+if command -v rustup > /dev/null 2>&1; then
+	echo "[INSTANLADO] - Google Chrome"
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+else
+	echo "[INSTALADO] - Rust"
+fi
