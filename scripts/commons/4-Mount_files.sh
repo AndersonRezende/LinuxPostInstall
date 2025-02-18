a199c0af15258a4ef98ee58b24de32728c4b1546
Anderson Rezende <andersonrezende17@hotmail.com>
2025-02-17 23:29:55 -0300

    Altera script de ponto de montagem para carregar por label

--- scripts/commons/4-Mount_files.sh
+++ scripts/commons/4-Mount_files.sh
@@ -2,8 +2,8 @@
 
 ######### PARTIÇÃO DE ARQUIVOS #########
 echo "[CONFIGURANDO] - Montagem da partição de Arquivos"
-DEVICE=/dev/nvme1n1p1
 MOUNT_DIR=/mnt/Arquivos
-echo "$DEVICE $MOUNT_DIR auto nosuid,nodev,nofail,x-gvfs-show,x-gvfs-name=Arquivos 0 0" | sudo tee -a /etc/fstab
+LABEL=ARQUIVOS
+echo "LABEL=$LABEL $MOUNT_DIR auto nosuid,nodev,nofail,x-gvfs-show,x-gvfs-name=Arquivos 0 0" | sudo tee -a /etc/fstab
 echo "[CONFIGURADO] - Montagem da partição de Arquivos"
 ######### PARTIÇÃO DE ARQUIVOS #########
