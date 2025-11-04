c439dcfcc7bcd05873fdaca7bc12de11246239a3
Anderson Rezende <andersonrezende17@hotmail.com>
2025-11-04 17:57:18 -0300

    Corrige nomenclatura de instalação do Rust

--- scripts/commons/7-Rust.sh
+++ scripts/commons/7-Rust.sh
@@ -3,7 +3,7 @@
 echo "Instalando RUST"
 
 if command -v rustup > /dev/null 2>&1; then
-	echo "[INSTANLADO] - Google Chrome"
+	echo "[INSTANLADO] - Rust"
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 else
 	echo "[INSTALADO] - Rust"
