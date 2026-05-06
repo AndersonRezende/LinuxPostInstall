1c6836cb89ada13354652db20819e94a88bd7695
Anderson Rezende <andersonrezende17@hotmail.com>
2026-05-05 23:35:24 -0300

    Introduce Jetbrains ToolBox script

---
+++ scripts/commons/9-jetbrains-toolbox.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Instalando JetBrains Toolbox"
+
+# Define a URL da API da JetBrains para obter o link da versão mais recente (Linux)
+URL="https://data.services.jetbrains.com/products/releases?code=TBA&latest=true&type=release"
+DOWNLOAD_URL=$(curl -s $URL | grep -oP '"linux":\s*\{\s*"link":\s*"\K[^"]+')
+
+mkdir -p /tmp/jetbrains-toolbox
+sudo mkdir -p /opt/jetbrains-toolbox
+curl -L "$DOWNLOAD_URL" -o /tmp/jetbrains-toolbox/toolbox.tar.gz >/dev/null 2>&1
+
+tar -xzf /tmp/jetbrains-toolbox/toolbox.tar.gz -C /opt/jetbrains-toolbox --strip-components=1 >/dev/null 2>&1
+/opt/jetbrains-toolbox/jetbrains-toolbox >/dev/null 2>&1 &
+
+echo "[INSTALADO] - JetBrains Toolbox"
