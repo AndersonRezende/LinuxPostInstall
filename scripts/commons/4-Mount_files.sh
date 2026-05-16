8e0a694caa09f489e455c275f76425278d99b838
Anderson Rezende <andersonrezende17@hotmail.com>
2026-05-16 19:19:32 -0300

    Prevent create duplicated fstab ARQUIVOS patition entry

--- scripts/commons/4-Mount_files.sh
+++ scripts/commons/4-Mount_files.sh
@@ -4,6 +4,10 @@
 echo "[CONFIGURANDO] - Montagem da partição de Arquivos"
 MOUNT_DIR=/mnt/Arquivos
 LABEL=ARQUIVOS
-echo "LABEL=$LABEL $MOUNT_DIR auto nosuid,nodev,nofail,x-gvfs-show,x-gvfs-name=Arquivos 0 0" | sudo tee -a /etc/fstab
+if grep -qE "^LABEL=$LABEL\b" /etc/fstab; then
+  echo "[PULAR] - LABEL=$LABEL já existe em /etc/fstab"
+else
+  echo "LABEL=$LABEL $MOUNT_DIR auto nosuid,nodev,nofail,x-gvfs-show,x-gvfs-name=Arquivos 0 0" | sudo tee -a /etc/fstab
+fi
 echo "[CONFIGURADO] - Montagem da partição de Arquivos"
 ######### PARTIÇÃO DE ARQUIVOS #########
