ddb01ab19274231a0daaeec7584b52ba4f2b9e60
Anderson Rezende <andersonrezende17@hotmail.com>
2026-05-21 22:43:43 -0300

    Fix instalando label

--- scripts/commons/7-Rust.sh
+++ scripts/commons/7-Rust.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-echo "Instalando RUST"
-
+echo -n "Instalando RUST"
+sleep 1
 if command -v rustup > /dev/null 2>&1; then
-	echo "[INSTANLADO] - Rust"
+	echo -e "\r[INSTALANDO] - Rust"
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 else
-	echo "[INSTALADO] - Rust"
+	echo -e "\r[INSTALADO] - Rust"
 fi
